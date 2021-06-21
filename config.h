1318127321e7dd895d456e71eceb1868395e52b6
Joinemm <joonas@rautiola.co>
2021-06-21 07:52:51 +0300

    change fonts

--- config.h
+++ config.h
@@ -16,9 +16,9 @@ static int smartgaps            = 0;        /* 1 means no outer gap when there i
 static int showbar              = 1;        /* 0 means no bar */
 static int topbar               = 1;        /* 0 means bottom bar */
 static char *fonts[]            = {
-	"monospace:size=12",
+	"JetBrains Mono:size=12",
 	"Twemoji:size=12",
-	"Font Awesome 5 Free Solid:size=12"
+	"Feather Icons:size=12",
 };
 static char normbgcolor[]		= "#222222";
 static char normbordercolor[]   = "#444444";
