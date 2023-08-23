1b68570c6f1156a75bff1617ad4d33894b4356c7
Joinemm <joonas@rautiola.co>
2023-08-23 16:12:04 +0300

    use wezterm

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* Constants */
-#define TERMINAL "kitty"
-#define TERMCLASS "Kitty"
+#define TERMINAL "wezterm"
+#define TERMCLASS "Wezterm"
 
 /* appearance */
 static unsigned int borderpx    = 3;        /* border pixel of windows */
